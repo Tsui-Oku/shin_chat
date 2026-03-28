539ace90bc55c432936da0ab82e0bf3ff7d1e08c
Tsui-Oku <senyoumode@gmail.com>
2026-03-28 23:45:53 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,13 @@
+const CACHE = 'shin-chat-v1';
+const FILES = [
+  '/shin_chat/',
+  '/shin_chat/index.html'
+];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
