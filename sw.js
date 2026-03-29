10c94ee6cef5573e6628fa40e7187fece6e1e92f
Tsui-Oku <senyoumode@gmail.com>
2026-03-29 09:18:52 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,24 +1,13 @@
-const CACHE = 'shin-chat';
-
+const CACHE = 'shin-chat-v1';
+const FILES = [
+  '/shin_chat/',
+  '/shin_chat/index.html'
+];
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(['/shin_chat/', '/shin_chat/index.html']))
-  );
-  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
 });
-
-self.addEventListener('activate', e => {
-  e.waitUntil(clients.claim());
-});
-
 self.addEventListener('fetch', e => {
   e.respondWith(
-    fetch(e.request)
-      .then(res => {
-        const copy = res.clone();
-        caches.open(CACHE).then(c => c.put(e.request, copy));
-        return res;
-      })
-      .catch(() => caches.match(e.request))
+    caches.match(e.request).then(r => r || fetch(e.request))
   );
 });
